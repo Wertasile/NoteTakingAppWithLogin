74131fde629ad286157bed6b9f3ccfcb42f62c39
Wertasile <am8704y@gre.ac.uk>
2024-03-23 12:46:28 +0000

    Application User Id of Tags Table was made a foreign key

--- NoteTakingAppWithLogin/Server/Data/Migrations/20240322010253_AddForeignkeyToApplicationUserId.cs
+++ NoteTakingAppWithLogin/Server/Data/Migrations/20240322010253_AddForeignkeyToApplicationUserId.cs
@@ -10,7 +10,14 @@ namespace NoteTakingAppWithLogin.Server.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            
+            migrationBuilder.Sql(@"
+
+        ALTER TABLE Tags
+        ALTER COLUMN ApplicationUserId NVARCHAR(450);
+
+        ALTER TABLE Tags
+        ADD CONSTRAINT FK_Tags_AspNetUsers_ApplicationUserId FOREIGN KEY (ApplicationUserId) 
+        REFERENCES AspNetUsers(Id) ON DELETE CASCADE;");
         }
 
         
